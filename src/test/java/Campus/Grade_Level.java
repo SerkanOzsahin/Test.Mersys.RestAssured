96c95398be750a86d44f1f2f845735402ec418f2
aycadogannn <aycauresin@gmail.com>
2023-11-05 23:31:17 +0300

    grade level almost done

---
+++ src/test/java/Campus/Grade_Level.java
@@ -0,0 +1,138 @@
+package Campus;
+
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Grade_Level extends Hooks{
+    String gradeLevelID = "";
+
+    private String gradeLevelName;
+
+    private String gradeLevelShortName;
+
+
+
+    @Test
+    public void createGradeLevel() {
+        String rndGradeLevelName = randomFaker.name().firstName();
+        String rndGradeLevelShortName = randomFaker.name().title();
+
+        gradeLevelName = rndGradeLevelName;
+        gradeLevelShortName = rndGradeLevelShortName;
+
+        Map<String, Object> newGradeLevel = new HashMap<>();
+        newGradeLevel.put("name", rndGradeLevelName);
+        newGradeLevel.put("shortName",rndGradeLevelShortName);
+        newGradeLevel.put("nextGradeLevel",null);
+        newGradeLevel.put("order", "1");
+        newGradeLevel.put("maxApplicationCount","5");
+        newGradeLevel.put("active", true);
+        newGradeLevel.put("schoolIds","646cbb07acf2ee0d37c6d984");
+        newGradeLevel.put("showToAllSchools", false);
+        newGradeLevel.put("enableForSelectedSchools", true);
+
+
+        gradeLevelID =
+
+                given()
+                        .spec(reqSpec)
+                        .body(newGradeLevel)
+
+                        .when()
+                        .post("school-service/api/grade-levels")
+
+                        .then()
+                        .contentType(ContentType.JSON)
+                        .statusCode(201)
+                        .extract().path("id");
+    }
+
+    @Test(dependsOnMethods = "createGradeLevel")
+    public void createGradeLevelNegative() {
+        Map<String, Object> newGradeLevel = new HashMap<>();
+        newGradeLevel.put("name", gradeLevelName);
+        newGradeLevel.put("shortName",gradeLevelShortName);
+        newGradeLevel.put("nextGradeLevel",null);
+        newGradeLevel.put("order", "1");
+        newGradeLevel.put("maxApplicationCount","5");
+        newGradeLevel.put("active", true);
+        newGradeLevel.put("schoolIds","646cbb07acf2ee0d37c6d984");
+        newGradeLevel.put("showToAllSchools", false);
+        newGradeLevel.put("enableForSelectedSchools", true);
+
+        given()
+                .spec(reqSpec)
+                .body(newGradeLevel)
+
+                .when()
+                .post("school-service/api/grade-levels")
+
+                .then()
+                .contentType(ContentType.JSON)
+                .statusCode(400)
+                .log().body();
+    }
+
+
+    @Test(dependsOnMethods = "createGradeLevelNegative")
+    public void updateGradeLevel() {
+        Map<String, Object> updateGradeLevel = new HashMap<>();
+        updateGradeLevel.put("id", gradeLevelID);
+        updateGradeLevel.put("name", "blair waldorf");
+        updateGradeLevel.put("shortName","bw");
+        updateGradeLevel.put("nextGradeLevel",null);
+        updateGradeLevel.put("order", "1");
+        updateGradeLevel.put("maxApplicationCount","5");
+        updateGradeLevel.put("active", true);
+        updateGradeLevel.put("schoolIds","646cbb07acf2ee0d37c6d984");
+        updateGradeLevel.put("showToAllSchools", false);
+        updateGradeLevel.put("enableForSelectedSchools", true);
+
+
+        given()
+                .spec(reqSpec)
+                .body(updateGradeLevel)
+
+                .when()
+                .put("school-service/api/grade-levels")
+
+                .then()
+                .contentType(ContentType.JSON)
+                .statusCode(200)
+                .body("name",equalTo("blair waldorf"));
+    }
+
+    @Test(dependsOnMethods = "updateGradeLevel")
+    public void deleteGradeLevel() {
+        given()
+                .spec(reqSpec)
+
+                .when()
+                .delete("school-service/api/grade-levels/" + gradeLevelID)
+
+                .then()
+                .statusCode(204);
+    }
+
+    @Test(dependsOnMethods = "deleteGradeLevel")
+    public void deleteGradeLevelNegative() {
+        given()
+                .spec(reqSpec)
+
+                .when()
+                .delete("school-service/api/grade-levels/" + gradeLevelID)
+
+                .then()
+                .statusCode(400)
+                .log().body();
+    }
+}
+
+
+
--- src/test/java/Campus/Positions.java
+++ src/test/java/Campus/Positions.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
     public class Positions extends Hooks {
@@ -26,9 +25,11 @@ import static org.hamcrest.Matchers.equalTo;
             positionName = rndPositionName;
             positionShortName = rndPositionShortName;
 
-            Map<String, String> newPosition = new HashMap<>();
+            Map<String, Object> newPosition = new HashMap<>();
             newPosition.put("name", rndPositionName);
             newPosition.put("shortName",rndPositionShortName);
+            newPosition.put("tenantId","646cb816433c0f46e7d44cb0");
+            newPosition.put("active", true);
 
             positionID =
 
@@ -41,15 +42,18 @@ import static org.hamcrest.Matchers.equalTo;
 
                             .then()
                             .contentType(ContentType.JSON)
-                            .statusCode(400)
+                            .statusCode(201)
                             .extract().path("id");
         }
 
         @Test(dependsOnMethods = "createPosition")
         public void createPositionNegative() {
-            Map<String, String> newPosition = new HashMap<>();
+            Map<String, Object> newPosition = new HashMap<>();
             newPosition.put("name", positionName);
             newPosition.put("shortName",positionShortName);
+            newPosition.put("tenantId","646cb816433c0f46e7d44cb0");
+            newPosition.put("active", true);
+
             given()
                     .spec(reqSpec)
                     .body(newPosition)
@@ -66,10 +70,12 @@ import static org.hamcrest.Matchers.equalTo;
 
         @Test(dependsOnMethods = "createPositionNegative")
         public void updatePosition() {
-            Map<String, String> updatePosition = new HashMap<>();
+            Map<String, Object> updatePosition = new HashMap<>();
             updatePosition.put("id", positionID);
             updatePosition.put("name", "welder");
             updatePosition.put("shortName", positionShortName);
+            updatePosition.put("tenantId","646cb816433c0f46e7d44cb0");
+            updatePosition.put("active", true);
 
             given()
                     .spec(reqSpec)
@@ -80,7 +86,7 @@ import static org.hamcrest.Matchers.equalTo;
 
                     .then()
                     .contentType(ContentType.JSON)
-                    .statusCode(400)
+                    .statusCode(200)
                     .body("name",equalTo("welder"));
         }
 
@@ -105,8 +111,8 @@ import static org.hamcrest.Matchers.equalTo;
                     .delete("school-service/api/employee-position/" + positionID)
 
                     .then()
-                    .statusCode(400)
-                    .body("message", containsString("not found"));;
+                    .statusCode(204)
+                    .log().body();
         }
     }
 
