7023c8cfed4883ec893cf860568aecc45621375b
NurhayatSoydan <141257552+NurhayatSoydan@users.noreply.github.com>
2023-11-06 15:10:50 +0300

    bankAccount done

--- src/test/java/Campus/Adding_New_Fields.java
+++ src/test/java/Campus/Adding_New_Fields.java
@@ -1,10 +1,51 @@
 package Campus;
 
-public class Adding_New_Fields extends Hooks{
+import com.github.javafaker.Faker;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
+import java.util.Map;
 
+public class Adding_New_Fields extends Hooks {
+    Faker randomFaker = new Faker();
+    String fieldsID = "";
+    private String fieldsName;
 
+    @Test
+    public void createFields() {
 
+        String rndFieldsName = randomFaker.name().firstName();
+
+        fieldsName = rndFieldsName;
+
+        Map<String, Object> newFields = new HashMap<>();
+        newFields.put("name", rndFieldsName);
+        newFields.put("active", true);
+        newFields.put("code", 411);
+        newFields.put("type", "STRING");
+        newFields.put("systemField", false);
+        newFields.put("systemFieldName", null);
+        newFields.put("schoolId", "646cbb07acf2ee0d37c6d984");
+
+        fieldsID =
+                given()
+                        .spec(reqSpec)
+                        .body(newFields)
+
+                        .when()
+                        .post("/school-service/api/entity-field")
+                        .then()
+                        .contentType(ContentType.JSON)
+                        .log().body()
+                        .statusCode(201)
+                        .extract().path("id")
+        ;
+
+    }
 
 }
